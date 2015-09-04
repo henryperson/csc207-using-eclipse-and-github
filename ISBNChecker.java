04199a5edb3867738c0b74ff3583cbad32ddc019
Henry Fisher <fisherhe@grinnell.edu>
2015-09-03 19:51:33 -0500

    Adding comments for brownie points

--- ISBNChecker.java
+++ ISBNChecker.java
@@ -1,28 +1,35 @@
 
 public class ISBNChecker {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println(isISBN("0205080057"));
-		System.out.println(isISBN("0136091812"));
-		System.out.println(isISBN("123456789X"));
-		System.out.println(isISBN("0070651191"));
-
-	}
-	public static boolean isISBN(String isbn) {
-		int sum = 0;
-		for (int i = 0; i < isbn.length() - 1; i++) {
-			sum += Character.getNumericValue(isbn.charAt(i)) * (i+1);
-		}
-		int remainder = sum % 11;
-		char lastDigit = isbn.charAt(9);
-		if (remainder == 10) {
-			return (lastDigit == 'X');
-		} else {
-			return (Character.getNumericValue(lastDigit) == remainder);
-		}	
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Example 1
+        System.out.println(isISBN("0205080057"));
+        // Example 2
+        System.out.println(isISBN("0136091812"));
+        // Example 3
+        System.out.println(isISBN("123456789X"));
+        // Example 4
+        System.out.println(isISBN("0070651191"));
+    }
+    
+    public static boolean isISBN(String isbn) {
+        int sum = 0; // Default sum for calculating ISBN sum.
+        // For loop used to iterate over ISBN and calculate sum.
+        for (int i = 0; i < isbn.length() - 1; i++) {
+            sum += Character.getNumericValue(isbn.charAt(i))*(i+1);
+        }
+        int remainder = sum % 11;
+        char lastDigit = isbn.charAt(9);
+        if (remainder == 10) {// If remainder is 10, check if last digit is X.
+            // This is for the ISBN system.
+            return (lastDigit == 'X');
+        } else {// Otherwise, we want the remainder to equal the last digit.
+            return (Character.getNumericValue(lastDigit)
+                    == remainder);
+        }	
+    }
 
 }
